232406ce8134ae5c73013d3e4dc7845f8e505697
Matt <matt@matthughes.dev>
2020-03-29 14:49:55 -0700

    announcements

--- frontend/src/components/leads/NewsFeed.js
+++ frontend/src/components/leads/NewsFeed.js
@@ -1,13 +1,62 @@
 import React, { Component, Fragment } from 'react';
-import { Card } from 'primereact/card';
+import { TabView, TabPanel } from 'primereact/tabview';
 import { TwitterTimelineEmbed } from 'react-twitter-embed';
+import '../styles/newsfeed.css';
 
 export class NewsFeed extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			activeIndex: 0
+		};
+	}
+
 	render() {
+		const announcements = (
+			<div>
+				<h1>Hello and Welcome!</h1>
+				<p>
+					This is where future information for the server will be placed. Keep
+					an eye out for useful tips and upcoming notices!
+				</p>
+				<p>
+					This is where future information for the server will be placed. Keep
+					an eye out for useful tips and upcoming notices!
+				</p>
+				<p>
+					This is where future information for the server will be placed. Keep
+					an eye out for useful tips and upcoming notices!
+				</p>
+			</div>
+		);
+
 		return (
 			<div>
 				<Fragment>
-					<Card title="Twitter Feed" style={{ fontSize: '20px' }}>
+					<TabView
+						activeIndex={this.state.activeIndex}
+						onTabChange={e => this.setState({ activeIndex: e.index })}
+						renderActiveOnly={false}
+					>
+						<TabPanel header="Announcements" contentStyle={{ height: '40vh' }}>
+							{announcements}
+						</TabPanel>
+						<TabPanel header="Twitter Feed">
+							<TwitterTimelineEmbed
+								sourceType="profile"
+								screenName="Arma3official"
+								options={{
+									width: '100%',
+									height: '39.5vh'
+								}}
+								theme="dark"
+								noHeader={true}
+								noBorders={true}
+								noFooter={true}
+							/>
+						</TabPanel>
+					</TabView>
+					{/* <Card title="Twitter Feed" style={{ fontSize: '20px' }}>
 						<TwitterTimelineEmbed
 							sourceType="profile"
 							screenName="Arma3official"
@@ -20,7 +69,7 @@ export class NewsFeed extends Component {
 							noBorders={true}
 							noFooter={true}
 						/>
-					</Card>
+					</Card> */}
 				</Fragment>
 			</div>
 		);
