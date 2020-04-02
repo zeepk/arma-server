27ff3fede35b3ea1b272bc1386b8c8e3d0f8beb2
Matt <matt@matthughes.dev>
2020-04-02 08:09:33 -0700

    navbar and image tags

--- frontend/src/components/leads/Tutorials.js
+++ frontend/src/components/leads/Tutorials.js
@@ -5,13 +5,9 @@ import '../styles/images.css';
 export class Tutorials extends Component {
 	render() {
 		return (
-			<div>
-				<Fragment>
-					<div className="imgContainer">
-						<div className="tag">Tutorials</div>
-						<img className="tut-image" src="/static/tutorials.jpg" />
-					</div>
-				</Fragment>
+			<div className="imgContainer">
+				<div className="tag">Tutorials</div>
+				<img className="tut-image" src="/static/tutorials.jpg" />
 			</div>
 		);
 	}
